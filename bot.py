aadb86bf8288fa57ca23a8afba3ffe15c96e6ee0
NikolayG <gn9377419582@gmail.comm>
2025-02-01 20:05:06 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,37 +1,52 @@
 from aiogram import Dispatcher, types, Bot
 from aiogram import F
 from aiogram.filters.command import Command
+from aiogram.types.callback_query import CallbackQuery
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config
 import asyncio
 
 dp = Dispatcher()
 
+
+
 @dp.message(Command('start'))
 async def start_command(message: types.Message) -> None:
     kb = [
-        [types.InlineKeyboardButton(text="Список курсов", callback_data='kurs_list')],
+        [types.InlineKeyboardButton(text="каталог", callback_data='kurs_list')],
 
         [
-            types.InlineKeyboardButton(text="Имя", callback_data='name'),
-            types.InlineKeyboardButton(text="Портфолио", callback_data='portfolio')
+            types.InlineKeyboardButton(text="профиль", callback_data='name'),
+            types.InlineKeyboardButton(text="тех. поддержка", callback_data='portfolio')
         ],
-        [types.InlineKeyboardButton(text="О тебе", callback_data='about_you')]
+        [types.InlineKeyboardButton(text="о нас", callback_data='about_as')]
     ]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
-    await message.answer('каталог', reply_markup=keyboard)
+    await message.answer(f'Привет {message.from_user.full_name} ты хочешь получить знания по python?', reply_markup=keyboard)
 
 
-
-# всплывающее окно
 @dp.callback_query(F.data == "kurs_list")
-async def about_me_callback(callback: types.CallbackQuery):
+async def kurs_list(callback: types.CallbackQuery):
     kb = [
         [types.InlineKeyboardButton(text = 'курс python',callback_data="python_curs"),types.InlineKeyboardButton(text = 'курс django',callback_data='django_curs')],
         [types.InlineKeyboardButton(text = 'курс c#',callback_data="c_curs"),types.InlineKeyboardButton(text = 'курс C++',callback_data='c_plus_curs')],
-    types.InlineKeyboardButton(text = 'на главную страницу',callback_data="start")]
+        [types.InlineKeyboardButton(text = 'на главную страницу',callback_data="start")]]
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
     await callback.message.edit_text('выберите список курсов', reply_markup=keyboard)
 
+@dp.callback_query(F.data == "start")
+async def start_command(callback: types.CallbackQuery) -> None:
+    kb = [
+        [types.InlineKeyboardButton(text="каталог", callback_data='kurs_list')],
+
+        [
+            types.InlineKeyboardButton(text="профиль", callback_data='name'),
+            types.InlineKeyboardButton(text="тех. поддержка", callback_data='portfolio')
+        ],
+        [types.InlineKeyboardButton(text="о нас", callback_data='about_as')]
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
+    await callback.message.edit_text('выберите список курсов', reply_markup=keyboard)
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN)
